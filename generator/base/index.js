9532e3a868473153fec68485ee086ac30436ea67
Alexander Schwartzberg <aeksco@gmail.com>
2018-11-20 21:48:07 -0500

    Generator API page - all API examples can toggle on/off via configuration

--- generator/base/index.js
+++ generator/base/index.js
@@ -1,26 +1,19 @@
-const Generator = require('@codotype/generator')
-
-// // // //
-
-module.exports = class ReactJsBase extends Generator {
-
-  compileInPlace () {
-    return [
-      'models/User.js',
-      'config/passport.js',
-      'views/layout.pug',
-      'views/home.pug',
-      'views/partials/header.pug',
-      'views/account/login.pug',
-      'app.js'
-    ]
-  }
-
+module.exports = {
+  name: 'HackathonStarterBase',
+  compileInPlace: [
+    'models/User.js',
+    'config/passport.js',
+    'views/layout.pug',
+    'views/home.pug',
+    'views/api/index.pug',
+    'views/partials/header.pug',
+    'views/account/login.pug',
+    'app.js'
+  ],
   async write () {
     await this.copyDir(
       this.templatePath(),
       this.destinationPath()
     )
   }
-
 }
