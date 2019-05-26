6b4ec9809c236aff4114db389881fbc307f4fbe8
Alexander Schwartzberg <aeksco@gmail.com>
2019-05-25 23:46:15 -0400

    Updated generator api examples

--- generator/api_examples/index.js
+++ generator/api_examples/index.js
@@ -2,80 +2,84 @@ module.exports = {
   name: 'HackathonStarterApiExamples',
   async write ({ configuration }) {
 
-    if (configuration.options.enable_github_api) {
+    if (configuration.api_examples.github) {
       await this.renderComponent({ src: 'github.pug', dest: 'views/api/github.pug' })
     }
 
-    if (configuration.options.enable_twitter_api) {
+    if (configuration.api_examples.twitter) {
       await this.renderComponent({ src: 'twitter.pug', dest: 'views/api/twitter.pug' })
     }
 
-    if (configuration.options.enable_facebook_api) {
+    if (configuration.api_examples.facebook) {
       await this.renderComponent({ src: 'facebook.pug', dest: 'views/api/facebook.pug' })
     }
 
-    if (configuration.options.enable_foursquare_api) {
+    if (configuration.api_examples.foursquare) {
       await this.renderComponent({ src: 'foursquare.pug', dest: 'views/api/foursquare.pug' })
     }
 
-    if (configuration.options.enable_instagram_api) {
+    if (configuration.api_examples.instagram) {
       await this.renderComponent({ src: 'instagram.pug', dest: 'views/api/instagram.pug' })
     }
 
-    if (configuration.options.enable_last_fm_api) {
+    if (configuration.api_examples.last_fm) {
       await this.renderComponent({ src: 'lastfm.pug', dest: 'views/api/lastfm.pug' })
     }
 
-    if (configuration.options.enable_linkedin_api) {
+    if (configuration.api_examples.linkedin) {
       await this.renderComponent({ src: 'linkedin.pug', dest: 'views/api/linkedin.pug' })
     }
 
-    if (configuration.options.enable_new_york_times_api) {
+    if (configuration.api_examples.new_york_times) {
       await this.renderComponent({ src: 'nyt.pug', dest: 'views/api/nyt.pug' })
     }
 
-    if (configuration.options.enable_steam_api) {
+    if (configuration.api_examples.steam) {
       await this.renderComponent({ src: 'steam.pug', dest: 'views/api/steam.pug' })
     }
 
-    if (configuration.options.enable_stripe_api) {
+    if (configuration.api_examples.stripe) {
       await this.renderComponent({ src: 'stripe.pug', dest: 'views/api/stripe.pug' })
     }
 
-    if (configuration.options.enable_paypal_api) {
+    if (configuration.api_examples.paypal) {
       await this.renderComponent({ src: 'paypal.pug', dest: 'views/api/paypal.pug' })
     }
 
-    if (configuration.options.enable_twilio_api) {
+    if (configuration.api_examples.twilio) {
       await this.renderComponent({ src: 'twilio.pug', dest: 'views/api/twilio.pug' })
     }
 
-    if (configuration.options.enable_tumblr_api) {
+    if (configuration.api_examples.tumblr) {
       await this.renderComponent({ src: 'tumblr.pug', dest: 'views/api/tumblr.pug' })
     }
 
-    if (configuration.options.enable_web_scraping_api) {
+    if (configuration.api_examples.web_scraping) {
       await this.renderComponent({ src: 'scraping.pug', dest: 'views/api/`scraping.pug' })
     }
 
-    if (configuration.options.enable_clockwork_sms_api) {
+    if (configuration.api_examples.clockwork_sms) {
       await this.renderComponent({ src: 'clockwork.pug', dest: 'views/api/clockwork.pug' })
     }
 
-    if (configuration.options.enable_aviary_api) {
+    if (configuration.api_examples.aviary) {
       await this.renderComponent({ src: 'aviary.pug', dest: 'views/api/aviary.pug' })
     }
 
-    if (configuration.options.enable_lob_api) {
+    if (configuration.api_examples.lob) {
       await this.renderComponent({ src: 'lob.pug', dest: 'views/api/lob.pug' })
     }
 
-    if (configuration.options.enable_pinterest_api) {
+    if (configuration.api_examples.pinterest) {
       await this.renderComponent({ src: 'pinterest.pug', dest: 'views/api/pinterest.pug' })
     }
 
-    if (configuration.options.enable_google_maps_api) {
+    if (configuration.api_examples.google_maps) {
       await this.renderComponent({ src: 'google-maps.pug', dest: 'views/api/google-maps.pug' })
+    }
+
+    if (configuration.api_examples.chartjs) {
+      await this.renderComponent({ src: 'chart.pug', dest: 'views/api/chart.pug' })
     }
 
   }
